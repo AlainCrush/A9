87d54340c2fc6b3ba7979144b74cfc6ff3358797
Administrator <1604825334@qq.com>
2021-04-27 08:31:29 +0800

    4.27日修改

--- src/main/java/com/example/a9/RateActivity.java
+++ src/main/java/com/example/a9/RateActivity.java
@@ -3,8 +3,12 @@ package com.example.a9;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,42 +16,67 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.Set;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class RateActivity extends AppCompatActivity {
+public class RateActivity extends AppCompatActivity implements Runnable {
     EditText et;
     TextView tw;
     String edit;
-    double dollarRate = 0.1528;
-    double euroRate =0.1284;
-    double wonRate = 171.8839;
-    double i = 0;
+    float dollarRate = 0.15f;
+    float euroRate =0.13f;
+    float wonRate = 171.9f;
+    float i = 0;
+    Handler handler;
+
+    private static final String TAG = "RateActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
         et = (EditText) findViewById(R.id.Et2);
         tw = findViewById(R.id.textView6);
+        Thread t = new Thread(this);
+        t.start();
+
+        handler =new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.what == 7) {
+                    String str = (String) msg.obj;
+                    Log.i(TAG, "handleMessage: get str" + str);
+                    tw.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
 
     }
+
     private void show(double s){
         tw.setText(String.valueOf(s));
     }
+
     public void dollar(View v){
         edit = et.getText().toString();
-         i =Double.parseDouble(edit);
+         i =Float.parseFloat(edit);
         i*=dollarRate;
         show(i);
     }
     public void won(View v){
         edit = et.getText().toString();
-         i =Double.parseDouble(edit);
+         i =Float.parseFloat(edit);
         i*=wonRate;
         show(i);
     }
     public void euro(View v){
         edit = et.getText().toString();
-         i =Double.parseDouble(edit);
+         i =Float.parseFloat(edit);
         i*=euroRate;
         show(i);
     }
@@ -62,11 +91,15 @@ public class RateActivity extends AppCompatActivity {
     }
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         if(requestCode==5 && resultCode==2){
-            Bundle bd1 =data.getExtras();
-            dollarRate = bd1.getDouble("key_dollarRate", 0.1528);
-            euroRate = bd1.getDouble("key_euroRate", 0.1284);
-            wonRate = bd1.getDouble("key_wonRate", 171.8839);
-
+           /* Bundle bd1 =data.getExtras();
+            dollarRate = bd1.getFloat("key_dollarRate", 0.1528);
+            euroRate = bd1.getFloat("key_euroRate", 0.1284);
+            wonRate = bd1.getFloat("key_wonRate", 171.8839);*/
+            //获取SharePreferences数据
+            SharedPreferences sp = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+            dollarRate = sp.getFloat("key_dollarRate",0.0f);
+            euroRate = sp.getFloat("key_euroRate",0.0f);
+            wonRate = sp.getFloat("key_wonRate",0.0f);
         }
         super.onActivityResult(requestCode,resultCode,data);
     }
@@ -86,5 +119,44 @@ public class RateActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+    private String inputStream2String(InputStream inputStream)
+            throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+    }
+
+    @Override
+    public void run() {
+        URL url =null;
+        try {
+            url =new URL("https://www.usd-cny.com/");
+            HttpURLConnection http =(HttpURLConnection)url.openConnection();
+            InputStream in =http.getInputStream();
+
+            String html =inputStream2String(in);
+            Log.i(TAG, "html="+html);
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Log.i(TAG, "run: ............");
+        //线程完成任务
+        //返回线程数据
+        Message msg = handler.obtainMessage(7);
+        msg.obj = "From message";
+        handler.sendMessage(msg);//发送消息
+
     }
 }
--- src/main/java/com/example/a9/activity_ratechange.java
+++ src/main/java/com/example/a9/activity_ratechange.java
@@ -2,7 +2,9 @@ package com.example.a9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,9 +23,9 @@ public class activity_ratechange extends AppCompatActivity {
         euro = findViewById(R.id.T2);
         won = findViewById(R.id.T3);
         Intent con = getIntent();
-        double dollar2 = con.getDoubleExtra("dollarRate", 0);
-        double euro2 = con.getDoubleExtra("euroRate", 0);
-        double won2 = con.getDoubleExtra("wonRate", 0);
+        float dollar2 = con.getFloatExtra("dollarRate", 0);
+        float euro2 = con.getFloatExtra("euroRate", 0);
+        float won2 = con.getFloatExtra("wonRate", 0);
         dollar.setText(String.valueOf(dollar2));
         euro.setText(String.valueOf(euro2));
         won.setText(String.valueOf(won2));
@@ -31,17 +33,24 @@ public class activity_ratechange extends AppCompatActivity {
     }
     public void save(View v){
         //获得输入的数据
-        double dollar1 =Double.parseDouble(dollar.getText().toString());
-        double euro1 =Double.parseDouble(euro.getText().toString());
-        double won1 =Double.parseDouble(won.getText().toString());
+        float dollar1 =Float.parseFloat(dollar.getText().toString());
+        float euro1 =Float.parseFloat(euro.getText().toString());
+        float won1 =Float.parseFloat(won.getText().toString());
 
         //返回数据
+        SharedPreferences sp = getSharedPreferences("myrate", Activity.MODE_PRIVATE);//创建SharePreferences对象用以保存数据
+        SharedPreferences.Editor editor = sp.edit();//获取编辑器
+        //在SharePreferences中上传数据
+        editor.putFloat("key_dollarRate",dollar1);
+        editor.putFloat("key_euroRate", euro1);
+        editor.putFloat("key_wonRate", won1);
+        editor.apply();//提交
         Intent intent =getIntent();
-        Bundle bd = new Bundle();//新建一个Bundle存放数据
-        bd.putDouble("key_dollarRate",dollar1);
-        bd.putDouble("key_euroRate",euro1);
-        bd.putDouble("key_wonRate",won1);
-        intent.putExtras(bd);
+        /*Bundle bd = new Bundle();//新建一个Bundle存放数据
+        bd.putFloat("key_dollarRate",dollar1);
+        bd.putFloat("key_euroRate",euro1);
+        bd.putFloat("key_wonRate",won1);
+        intent.putExtras(bd);*/
         setResult(2,intent);//设置resultCode并带回数据
         //返回调用页面
         finish();
